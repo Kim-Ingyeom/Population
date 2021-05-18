8374098e038941e73f45df92b5099be3cd1b45ce
kingoneju <parkhajh0803@naver.com>
2021-05-18 15:41:14 +0900

    graph1

--- population.py
+++ population.py
@@ -1,6 +1,7 @@
 import time
 import selenium
 from selenium import webdriver
+import matplotlib.pyplot as plt
 
 driver = webdriver.Chrome()
 
@@ -12,17 +13,29 @@ test=driver.find_elements_by_tag_name("td")
 count=0
 new_list_x =[]
 new_list_y =[]
-
+list_y=[]
 for i in test[0:20]:
     if count%4 ==1:
          new_list_x.append(i.text)
     elif count%4 == 2:
-         new_list_y.append(i.text)
+
+        list_y.append(i.text)
     count+=1
 
+for i in list_y:
+    i = int(i.replace(',',''))
+    new_list_y.append(i)
+
+x=[1,2,3,4,5]
 
 print(new_list_x)
 print(new_list_y)
+
+plt.plot(x, new_list_y)
+
+plt.xticks(x,new_list_x)
+plt.show()
+
 
 driver.get('https://superkts.com/population/sum_sido/?o=3')
 
