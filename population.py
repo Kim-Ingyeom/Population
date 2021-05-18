7dfe82cc9b2a5c8107260d451feb74c1bb25d301
kingoneju <parkhajh0803@naver.com>
2021-05-18 15:16:49 +0900

    'crawlering'

--- population.py
+++ population.py
@@ -4,6 +4,42 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get('https://superkts.com/population/sum_sido/')
+driver.get('https://superkts.com/population/sum_sido/?o=2')
 
-time.sleep(3)
+time.sleep(1)
+
+test=driver.find_elements_by_tag_name("td")
+count=0
+new_list_x =[]
+new_list_y =[]
+
+for i in test[0:20]:
+    if count%4 ==1:
+         new_list_x.append(i.text)
+    elif count%4 == 2:
+         new_list_y.append(i.text)
+    count+=1
+
+
+print(new_list_x)
+print(new_list_y)
+
+driver.get('https://superkts.com/population/sum_sido/?o=3')
+
+time.sleep(1)
+
+test=driver.find_elements_by_tag_name("td")
+count=0
+new_list_x =[]
+new_list_y =[]
+
+for i in test[0:20]:
+    if count%4 ==1:
+         new_list_x.append(i.text)
+    elif count%4 == 2:
+         new_list_y.append(i.text)
+    count+=1
+
+
+print(new_list_x)
+print(new_list_y)
